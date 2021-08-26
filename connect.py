b303d9b226d200bb0ca9db4675ff619ca3012fa3
Luis Mendes <machadoluismendes@gmail.com>
2021-08-26 09:03:50 -0300

    MONGODB_URL

--- connect.py
+++ connect.py
@@ -1,4 +1,15 @@
 # cloud mongodb https://docs.atlas.mongodb.com/troubleshoot-connection/
 
+from github import Github
+
+# First create a Github instance:
+
+# using an access token
+g = Github("repo-token")
+
+MONGO_URL = Github("ream-secret")
+
+# def getMongoUrl() return g.get_user();
+
 # client = pymongo.MongoClient("mongodb+srv://fail:<password>@cluster0.neyhi.gcp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
 # db = client.test
--- github_octokit_consumer.py
+++ github_octokit_consumer.py
@@ -5,6 +5,8 @@ from github import Github
 # using an access token
 g = Github("repo-token")
 
+
+
 # Github Enterprise with custom hostname
 # g = Github(base_url="https://{hostname}/api/v3", login_or_token="repo-token")
 
