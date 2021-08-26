87f9243aac0312980f29b05d01cc637031e75b1c
Luis Mendes <machadoluismendes@gmail.com>
2021-08-26 07:51:48 -0300

    leaflet token

---
+++ connect.py
@@ -0,0 +1,4 @@
+# cloud mongodb https://docs.atlas.mongodb.com/troubleshoot-connection/
+
+# client = pymongo.MongoClient("mongodb+srv://fail:<password>@cluster0.neyhi.gcp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+# db = client.test
